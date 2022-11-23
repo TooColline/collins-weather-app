9c587bae25abcc1c43d22639982754aeea3451d2
TooColline <38856515+TooColline@users.noreply.github.com>
2022-11-23 21:10:14 +0300

    bring back cors info

--- src/main.ts
+++ src/main.ts
@@ -10,9 +10,9 @@ const app = createApp(App)
 app.use(router)
 app.use(store, key)
 
-//http://0.0.0.0:8080 for local bypass cors proxy
+//http://0.0.0.0:8080 or https://collins-bypass-proxy.herokuapp.com for local bypass cors proxy
 
-let baseURL = 'https://collins-bypass-proxy.herokuapp.com/https://api.openweathermap.org/data/2.5'
+let baseURL = 'https://cors-anywhere.herokuapp.com/corsdemo/https://api.openweathermap.org/data/2.5'
 axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*'
 axios.defaults.baseURL = app.config.globalProperties.publicPath = baseURL
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
